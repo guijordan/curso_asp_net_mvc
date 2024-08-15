5d850d6e7a027d296f49970d663547cf7f164d8c
Guilherme Jordan De Oliveira <guijordanoliveira@hotmail.com>
2024-08-15 19:23:36 -0300

    Aprendendo a usar migrations com o Entity Framework Core

--- LanchesMac/LanchesMac/Models/Categoria.cs
+++ LanchesMac/LanchesMac/Models/Categoria.cs
@@ -1,11 +1,24 @@
-﻿namespace LanchesMac.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LanchesMac.Models
 {
+    [Table("Categorias")]
     public class Categoria
     {
+        [Key]
         public int CategoriaId { get; set; }
-        public string CategoriaNome { get; set; }
-        public string Descricao { get; set; }
-        public List<Lanche> Lanches { get; set; }
 
+        [StringLength(100, ErrorMessage = "O tamanho máximo é 100 caracteres")]
+        [Required(ErrorMessage = "Informe o nome da categoria")]
+        [Display(Name = "Nome")]
+        public string CategoriaNome { get; set; }
+
+        [StringLength(200, ErrorMessage = "O tamanho máximo é 200 caracteres")]
+        [Required(ErrorMessage = "Informe a descrição da categoria")]
+        [Display(Name = "Descrição")]
+        public string Descricao { get; set; }
+
+        public List<Lanche> Lanches { get; set; }
     }
 }
