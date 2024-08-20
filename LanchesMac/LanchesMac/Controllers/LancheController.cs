0c4a5a3d4e4b2aa1fecaec5c365e0a15bd295f2a
Guilherme Jordan De Oliveira <guijordanoliveira@hotmail.com>
2024-08-20 19:42:49 -0300

    Aprendendo a usar as partialViews e como definir um modelo para as partial views para preencher as informações da página.

--- LanchesMac/LanchesMac/Controllers/LancheController.cs
+++ LanchesMac/LanchesMac/Controllers/LancheController.cs
@@ -1,4 +1,5 @@
 ﻿using LanchesMac.Repositories.Interfaces;
+using LanchesMac.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LanchesMac.Controllers
@@ -13,8 +14,12 @@ namespace LanchesMac.Controllers
 
         public IActionResult List()
         {
-            var lanches = _lancheRepository.Lanches;
-            return View(lanches);
+            //var lanches = _lancheRepository.Lanches;
+            //return View(lanches);
+            var lanchesListViewModel = new LancheListViewModel();
+            lanchesListViewModel.Lanches = _lancheRepository.Lanches;
+            lanchesListViewModel.CategoriaAtual = "Categoria Atual";
+            return View(lanchesListViewModel);
         }    
     }
 }
