e51b15b80bacd234b8c9f06046ab9158bfa12ab6
Guilherme Jordan De Oliveira <guijordanoliveira@hotmail.com>
2024-09-24 19:28:25 -0300

    Criando o formulário de login e de registro e utilização do framework Identity para efetuar o registro e o login do usuário no sistema.

--- LanchesMac/LanchesMac/Startup.cs
+++ LanchesMac/LanchesMac/Startup.cs
@@ -24,6 +24,16 @@ public class Startup
             .AddEntityFrameworkStores<AppDbContext>()
             .AddDefaultTokenProviders();
 
+        //services.Configure<IdentityOptions>(options =>
+        //{
+        //    options.Password.RequireDigit = false;
+        //    options.Password.RequireLowercase = false;
+        //    options.Password.RequireNonAlphanumeric = false;
+        //    options.Password.RequireUppercase = false;
+        //    options.Password.RequiredLength = 3;
+        //    options.Password.RequiredUniqueChars = 1;
+        //});
+
         services.AddTransient<ILancheRepository, LancheRepository>();
         services.AddTransient<ICategoriaRepository, CategoriaRepository>();
         services.AddTransient<IPedidoRepository, PedidoRepository>();
